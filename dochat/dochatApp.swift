16c0c0e53cd17f0a98b39e69ffdef9496a326527
deeptalutmahamud <mddeep22@gmail.com>
2024-01-09 15:32:33 +0600

    Initial Commit

---
+++ dochat/dochatApp.swift
@@ -0,0 +1,17 @@
+//
+//  dochatApp.swift
+//  dochat
+//
+//  Created by Talut mahamud Deep on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct dochatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
